275289a24be8c3f426e7bb744046e0d867db5c52
Christwelve <meng.christian28@gmail.com>
2023-11-28 14:30:32 +0100

    update

--- cherno/main.cpp
+++ cherno/main.cpp
@@ -3,17 +3,36 @@
 
 class Log 
 {
+	public:
+		const int LogLevelError = 0;
+		const int LogLevelWarning = 1;
+		const int LogLevelInfo = 2;
+
 	private:
-		int m_LogLevel;
+		int m_LogLevel = LogLevelInfo;
 
 	public:
 		void SetLevel(int level)
 		{
-
+			m_LogLevel = level;
 		}
+
+		void Error(const char* message)
+		{
+			if (m_LogLevel >= LogLevelError)
+				std::cout << "[ERROR]: " << message << std::endl;
+		}
+
 		void Warn(const char* message)
 		{
-			
+			if (m_LogLevel >= LogLevelWarning)
+				std::cout << "[WARNING]: " << message << std::endl;
+		}
+
+		void Info(const char* message)
+		{
+			if (m_LogLevel >= LogLevelInfo)
+				std::cout << "[INFO]: " << message << std::endl;
 		}
 
 };
@@ -21,7 +40,7 @@ class Log
 int main()
 {
 	Log log;
-	log.SetLevel(1);
+	log.SetLevel(log.LogLevelWarning);
 	log.Warn("Hello!");
 
 	std::cin.get();
