8f8800ea3387c3352b2fe7dc6720554b5b660e1c
Christwelve <meng.christian28@gmail.com>
2023-12-11 09:12:55 +0100

    cpp01 ex04

--- cpp01/ex04/Replace.cpp
+++ cpp01/ex04/Replace.cpp
@@ -6,7 +6,7 @@
 /*   By: cmeng <cmeng@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/30 06:00:29 by cmeng             #+#    #+#             */
-/*   Updated: 2023/12/08 17:43:23 by cmeng            ###   ########.fr       */
+/*   Updated: 2023/12/11 09:10:10 by cmeng            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,4 +19,30 @@ Replace::Replace(std::string filename, std::string s1, std::string s2)
 
 Replace::~Replace() { return; }
 
-void Replace::replace() { std::fstream fs; }
+void Replace::replace() {
+    std::fstream inFile(filename_);
+    if (!inFile) {
+        std::cerr << "Can't open file" << filename_ << std::endl;
+        return;
+    }
+
+    std::ofstream outFile(filename_ + ".replace");
+    if (!outFile) {
+        std::cerr << "Can't open file " << filename_ + ".replace" << std::endl;
+        return;
+    }
+
+    std::string line;
+    while (std::getline(inFile, line)) {
+        size_t pos = 0;
+        while ((pos = line.find(s1_, pos)) != std::string::npos) {
+            line.erase(pos, s1_.length());
+            line.insert(pos, s2_);
+            pos += s2_.length();
+        }
+        outFile << line << "\n";
+    }
+
+    inFile.close();
+    outFile.close();
+}
--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -6,8 +6,19 @@
 /*   By: cmeng <cmeng@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/30 05:49:26 by cmeng             #+#    #+#             */
-/*   Updated: 2023/12/08 17:42:43 by cmeng            ###   ########.fr       */
+/*   Updated: 2023/12/11 09:12:16 by cmeng            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int main(void) { return (0); }
+#include "Replace.hpp"
+
+int main(int argc, char **argv) {
+    if (argc != 4) {
+        std::cerr << "Wrong syntax: ./Replace filename s1 s2" << std::endl;
+        return (1);
+    }
+
+    Replace Test(argv[1], argv[2], argv[3]);
+    Test.replace();
+    return (0);
+}
