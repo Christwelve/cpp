e6819d0d788169b71985e44d2d7718eec3cf308a
Christwelve <meng.christian28@gmail.com>
2024-01-13 20:06:39 +0100

    added testcase

--- cpp07/ex02/main.cpp
+++ cpp07/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: cmeng <cmeng@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/11 13:33:14 by cmeng             #+#    #+#             */
-/*   Updated: 2024/01/11 15:27:46 by cmeng            ###   ########.fr       */
+/*   Updated: 2024/01/13 19:55:20 by cmeng            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,11 @@ int main() {
     std::cout << "Original array size:  " << intArray.size() << std::endl;
     Array<int> copiedArray = intArray;
     std::cout << "Copied array size:    " << copiedArray.size() << std::endl;
+
+    Array<std::string> strArray(5);
+    std::cout << "Original string array size:  " << strArray.size() << std::endl;
+    Array<std::string> copiedStrArray = strArray;
+    std::cout << "Copied string array size:    " << copiedStrArray.size() << std::endl;
 
     return (0);
 }
