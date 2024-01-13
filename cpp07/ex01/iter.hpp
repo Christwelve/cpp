9f14db9d806a565455c808ee041ada635cc00092
Christwelve <meng.christian28@gmail.com>
2024-01-13 21:28:27 +0100

    add function template in ex01

--- cpp07/ex01/iter.hpp
+++ cpp07/ex01/iter.hpp
@@ -6,15 +6,15 @@
 /*   By: cmeng <cmeng@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/11 08:45:03 by cmeng             #+#    #+#             */
-/*   Updated: 2024/01/11 08:48:06 by cmeng            ###   ########.fr       */
+/*   Updated: 2024/01/13 21:12:11 by cmeng            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ITER_HPP
 #define ITER_HPP
 
-template <typename T>
-void iter(T *array, int length, void (*func)(T const &)) {
+template <typename T, typename Function>
+void iter(T *array, int length, Function func) {
     for (int i = 0; i < length; i++) {
         func(array[i]);
     }
